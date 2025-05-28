374a5aab8147e1eee193cbde75998117943d0761
Murillo Aragão <murilloaragaao@gmail.com>
2025-05-28 01:19:29 -0300

    Finishing registration file w/ exception

--- backendphp/entity/Perfil.php
+++ backendphp/entity/Perfil.php
@@ -1,12 +1,14 @@
 <?php
 
 //classe enum
-class Perfil {
+class Perfil
+{
     const USUARIO = 1;
     const ADMINISTRADOR = 2;
 
     // garante que os perfis fixos existam na tabela tipo_perfil no banco de dados mysql
-    public static function garantirPerfisFixos($conn) {
+    public static function garantirPerfisFixos($conn)
+    {
         $sql = "SELECT COUNT(*) as total FROM tipo_perfil";
         $result = $conn->query($sql);
         $row = $result->fetch_assoc();
@@ -15,16 +17,16 @@ class Perfil {
         }
     }
     // retorna a descriçao do perfil baseado no id usando if
-    public static function descricao($id) {
+    public static function descricao($id)
+    {
         // self refere-se à própria classe Perfil
         // self::CONSTANTE para acessar as constantes da classe
         if ($id == self::USUARIO) {
             return 'Usuário';
-        } 
+        }
         if ($id == self::ADMINISTRADOR) {
             return 'Administrador';
         }
         return 'Desconhecido';
     }
 }
-
