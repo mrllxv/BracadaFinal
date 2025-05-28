9d863376854c15ba8e96a3c50ef51ec8bf5dfbfa
Murillo Aragão <murilloaragaao@gmail.com>
2025-05-28 16:06:50 -0300

    Update files + isAdmin function

--- backendphp/models/cadastro.php
+++ backendphp/models/cadastro.php
@@ -17,7 +17,7 @@ if (isset($_POST['nome'], $_POST['email'], $_POST['senha'], $_POST['data_nascime
 
     try {
         $conn = connect();
-
+        //verificando se ja existe um usuario com esse email
         $stmt = $conn->prepare("SELECT id_usuario FROM usuario WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -27,15 +27,17 @@ if (isset($_POST['nome'], $_POST['email'], $_POST['senha'], $_POST['data_nascime
             exit;
         }
         $usuario = new User(
-            0,
+            0,  //o id será gerado automaticamente pelo banco mysql (AUTO_INCREMENT)
             $nome,
             $email,
             $senha,
             new DateTime($data_nascimento),
-            Perfil::USUARIO,
+            Perfil::USUARIO, //perfil padrão para usuario
             false
         );
 
+        //inserindo no banco de dos mysql
+        //stmt novamente e bind_param
         $stmtInsert = $conn->prepare("INSERT INTO usuario (nome, email, senha, data_nascimento, cod_tipo_perfil) VALUES (?, ?, ?, ?, ?)");
         $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
         $data = $usuario->getDataNascimento()->format('Y-m-d');
