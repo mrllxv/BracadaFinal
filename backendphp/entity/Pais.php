c044f1c391e359f025276ccdafa2108bf9c685d1
Murillo Aragão <murilloaragaao@gmail.com>
2025-05-28 01:56:33 -0300

    Add (Medal, Country, Medal Type)

---
+++ backendphp/entity/Pais.php
@@ -0,0 +1,36 @@
+<?php
+class Pais{
+    private int $id_pais;
+    private string $nome;
+
+    private string $bandeira;
+
+    public function __construct(int $id_pais, string $nome, string $bandeira ){
+        $this->id_pais = $id_pais;
+        $this->nome = $nome;
+        $this->bandeira = $bandeira;
+    }
+
+    public function getId()
+    {
+        return $this->id_pais;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getBandeira(){
+        return $this->bandeira;
+    }
+
+    public function setNome(string $nome){
+        $this->nome = $nome;
+    }
+
+    public function setBandeira(string $bandeira){
+        $this->bandeira = $bandeira;
+    }
+
+
+}
